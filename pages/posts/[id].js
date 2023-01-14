564d1559af69b7ebb23b0db899f76b73c89186b7
John Bogil <johncbogil@gmail.com>
2023-01-14 12:21:13 -0500

    generate rand id

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -4,13 +4,13 @@ import Date from "../../components/date"
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import utilStyles from '../../styles/utils.module.css';
 
-export async function getStaticPaths() {
-    const paths = getAllPostIds();
-    return {
-        paths,
-        fallback: false
-    };
-}
+// export async function getStaticPaths() {
+//     const paths = getAllPostIds();
+//     return {
+//         paths,
+//         fallback: false
+//     };
+// }
 
 export async function getStaticProps({params}) {
     const postData = await getPostData(params.id);
